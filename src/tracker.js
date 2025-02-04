bceed04639b79595f4d4290eb6fb537e9dc36c78
rajeevkumar <rajeevkumar@newrelic.com>
2025-02-04 12:27:26 +0530

    fix: Upgrading dependencies of webpack

--- src/tracker.js
+++ src/tracker.js
@@ -69,11 +69,27 @@ export default class ThePlatformTracker extends nrvideo.VideoTracker {
   }
 
   getPlayerVersion () {
-    if ($pdk) return $pdk.version.toString()
+    if ($pdk) {
+      const versionString = $pdk.version.toString();
+      const versionMatch = versionString.match(/^(\d+\.\d+\.\d+)/);
+      return versionMatch ? versionMatch[1] : versionString;
+    }
   }
 
   isMuted () {
     return this.muted
+  }
+
+  getInstrumentationProvider() {
+    return 'New Relic';
+  }
+
+  getInstrumentationName() {
+    return this.getPlayerName();
+  }
+
+  getInstrumentationVersion() {
+    return this.getPlayerVersion();
   }
 
   initAdTracker () {
@@ -211,7 +227,7 @@ export default class ThePlatformTracker extends nrvideo.VideoTracker {
       this.src = e.data.clip.URL || bc.URL
       this.title = e.data.clip.title
       this.duration = e.data.clip.mediaLength
-      this.sendError({ errorMessage: e.data.friendlyMessage, errorDetail: e.data.message })
+      this.sendError({ errorMessage: e.data.friendlyMessage, errorDetail: e.data.message, errorCode: e.data.responseCode, errorName: e.data.title })
     } else { // Ad
       this.sendStart()
     }
